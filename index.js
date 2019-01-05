fb7346072d0f8468473d4e42ae086294d36c232b
Chris <c.d.harris.dedoc@gmail.com>
2019-01-05 12:08:41 +1100

    InProgress#_Medical

---
+++ index.js
@@ -0,0 +1,8 @@
+// var protBtn = document.getElementById('protBtn');
+// var linkApp = document.getElementsByClassName(".prototypeLink");
+
+// protBtn.addEventListener('click', goToApp)
+
+// function goToApp() {
+//     protBtn.onclick = linkApp; 
+// }
